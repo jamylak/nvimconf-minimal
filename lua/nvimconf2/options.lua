e7b617d93b3c013056236e60b5b29819022aaded
jamylak <jamylak@gmail.com>
2026-03-31 09:21:35 +1100

    options

---
+++ lua/nvimconf2/options.lua
@@ -0,0 +1,15 @@
+local opt = vim.opt
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.number = true
+opt.mouse = ''
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+opt.updatetime = 200
+opt.timeoutlen = 300
+opt.splitbelow = true
+opt.splitright = true
+
+vim.opt.shadafile = 'NONE'
