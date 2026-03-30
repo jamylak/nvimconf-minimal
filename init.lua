691b3c9df23323b7f67657bae8858ecf252aeec0
jamylak <jamylak@gmail.com>
2026-03-31 09:18:20 +1100

    basic options

---
+++ init.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.go.loadplugins = false
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+local opt = vim.opt
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.number = true
+opt.mouse = ''
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+opt.updatetime = 200
+opt.timeoutlen = 300
+opt.splitbelow = true
+opt.splitright = true
