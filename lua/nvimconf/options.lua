6f95325edbe4fe47f7a9b68017aa6a58f0c0d78d
jamylak <jamylak@gmail.com>
2026-04-08 17:53:36 +1000

    Keep default statusline with Neovim 0.12 ui2

--- lua/nvimconf/options.lua
+++ lua/nvimconf/options.lua
@@ -15,15 +15,10 @@ opt.splitright = true
 opt.cursorline = true
 opt.signcolumn = "yes"
 
--- ui2 in 0.12 makes cmdheight=0 viable enough to hide the bottom bar
+-- ui2 in 0.12 makes cmdheight=0 viable enough to hide the command area
 -- without falling back to the old press-enter message flow.
 if is_nvim_012 then
-  opt.laststatus = 0
   opt.cmdheight = 0
-  opt.showmode = false
-  opt.showcmd = false
-  opt.ruler = false
-  opt.shortmess:append("sSWcCq")
 end
 
 vim.opt.shadafile = "NONE"
